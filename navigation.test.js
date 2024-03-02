b49c791908462459204dce39bbb6c206a576cd33
mehra-sourav <1997mehra@gmail.com>
2024-03-02 14:58:57 +0530

    Added first basic test

--- navigation.test.js
+++ navigation.test.js
@@ -1,3 +1,27 @@
-test("Test runs", () => {
-  expect(2).toBe(2);
+const calculateCoordinates = require("./navigation");
+
+const initialCoords = {
+  x: 0,
+  y: 0,
+  z: 0,
+  direction: "E",
+};
+
+beforeEach(() => {
+  initialCoords.x = {
+    x: 0,
+    y: 0,
+    z: 0,
+    direction: "E",
+  };
+});
+
+test("Spacecraft moves along X axis", () => {
+  const commands = ["f", "f", "b", "b", "b"];
+  const { x, y, z, direction } = calculateCoordinates(initialCoords, commands);
+
+  expect(x).toBe(-1);
+  expect(y).toBe(0);
+  expect(z).toBe(0);
+  expect(direction).toBe("E");
 });
